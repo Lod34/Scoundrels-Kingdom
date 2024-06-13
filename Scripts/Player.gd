80e3d93c27c51138347f2c9022b1a25b8cdd2f63
Lod_34 <lorenzoadacher@gmail.com>
2024-06-13 15:21:36 +0200

    Movement updated
    now the movement works in both directions with the corresponding animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,28 @@
 extends CharacterBody2D
 
+var speed = 100
+
+var player_state
+
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-	velocity = direction * 600
+	
+	if direction.x == 0 and direction.y == 0:
+		player_state = "idle"
+	elif direction.x != 0.0 or direction.y != 0:
+		player_state = "running"
+		
+	velocity = direction * speed
 	move_and_slide()
 
-	if velocity.length() > 0.0:
-		get_node("Knight").play_walk_animation()
-	else:
-		get_node("Knight").play_walk_animation()
-		
+	play_animation(direction)
+	
+func play_animation(dir):
+	if player_state == "idle":
+		$AnimationPlayer.play("idle")
+	
+	if player_state == "running":
+		if dir.x == 1:
+			$AnimationPlayer.play("run_right")
+		if dir.x == -1:
+			$AnimationPlayer.play("run_left")
