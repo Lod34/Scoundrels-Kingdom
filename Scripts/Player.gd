f2d8e20383ebbfc07b35a81117fed871a7f55022
Lod_34 <lorenzoadacher@gmail.com>
2024-07-15 13:18:16 +0200

    Movement completed
    FIx movement's bugs and added all the animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-var speed = 100
-
+var speed = 250
+var face
 var player_state
 
 func _physics_process(delta):
@@ -14,15 +14,35 @@ func _physics_process(delta):
 		
 	velocity = direction * speed
 	move_and_slide()
-
-	play_animation(direction)
+	check_face()
+	play_animation(direction,face)
 	
-func play_animation(dir):
+func check_face():
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	if direction.x > 0:
+		face = "right"
+	if direction.x < 0:
+		face = "left"
+	return face
+
+func play_animation(dir,face):
 	if player_state == "idle":
-		$AnimationPlayer.play("idle")
+		if face == "right":
+			$AnimationPlayer.play("idle_right")
+		if face == "left":
+			$AnimationPlayer.play("idle_left")
 	
 	if player_state == "running":
 		if dir.x == 1:
 			$AnimationPlayer.play("run_right")
 		if dir.x == -1:
 			$AnimationPlayer.play("run_left")
+		
+		#play run animation in y direction
+		if dir.y != 0:
+			if face == "right":
+				$AnimationPlayer.play("run_right")
+			if face == "left":
+				$AnimationPlayer.play("run_left")
+
